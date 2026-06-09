bb66e2337be1b1ec265d9d43a1911f47346decbb
ciaabcdefg <ciaabcdefg@gmail.com>
2026-06-09 15:59:49 +0700

    invoker for LivingEntity

---
+++ src/main/java/ciaabcdefg/oeam/mixin/LivingEntityInvoker.java
@@ -0,0 +1,11 @@
+package ciaabcdefg.oeam.mixin;
+
+import net.minecraft.world.entity.LivingEntity;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.gen.Invoker;
+
+@Mixin(LivingEntity.class)
+public interface LivingEntityInvoker {
+    @Invoker("getCurrentSwingDuration")
+    int invokeGetCurrentSwingDuration();
+}
