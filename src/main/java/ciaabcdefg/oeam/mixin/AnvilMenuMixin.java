ee0c6624a3b0beee8890bdaddc92cd89387d0206
ciaabcdefg <ciaabcdefg@gmail.com>
2026-06-09 18:25:15 +0700

    precedence works for gr. sharp and vanilla sharp as well

--- src/main/java/ciaabcdefg/oeam/mixin/AnvilMenuMixin.java
+++ src/main/java/ciaabcdefg/oeam/mixin/AnvilMenuMixin.java
@@ -4,11 +4,13 @@ import ciaabcdefg.oeam.OPEnchantsAndMore;
 import ciaabcdefg.oeam.enchantment.ModEnchantments;
 import com.llamalad7.mixinextras.sugar.Local;
 import net.minecraft.core.Holder;
+import net.minecraft.resources.ResourceKey;
 import net.minecraft.world.inventory.AnvilMenu;
 import net.minecraft.world.item.enchantment.Enchantment;
 import net.minecraft.world.item.enchantment.Enchantments;
 import net.minecraft.world.item.enchantment.ItemEnchantments;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.Redirect;
@@ -28,21 +30,8 @@ public class AnvilMenuMixin {
             )
     )
     private void test(CallbackInfo ci, @Local(name = "enchantments") ItemEnchantments.Mutable enchantments) {
-        AtomicReference<Holder<Enchantment>> greaterEfficiency = new AtomicReference<>();
-        AtomicReference<Holder<Enchantment>> efficiency = new AtomicReference<>();
-
-        enchantments.keySet().forEach(holder -> {
-            if (holder.is(ModEnchantments.GREATER_EFFICIENCY)) {
-                greaterEfficiency.set(holder);
-            } else if (holder.is(Enchantments.EFFICIENCY)) {
-                efficiency.set(holder);
-            }
-        });
-
-        if (greaterEfficiency.get() != null && efficiency.get() != null) {
-            OPEnchantsAndMore.LOGGER.info("Has both GrEff and Eff - removing Eff");
-            enchantments.set(efficiency.get(), 0);
-        }
+        enchantmentsPrecedence(enchantments, ModEnchantments.GREATER_EFFICIENCY, Enchantments.EFFICIENCY);
+        enchantmentsPrecedence(enchantments, ModEnchantments.GREATER_SHARPNESS, Enchantments.SHARPNESS);
     }
 
     @Redirect(
@@ -53,9 +42,38 @@ public class AnvilMenuMixin {
             )
     )
     private boolean redirectAreCompatible(Holder<Enchantment> enchantment, Holder<Enchantment> other) {
-        if ((enchantment.is(ModEnchantments.GREATER_EFFICIENCY) && other.is(Enchantments.EFFICIENCY))
-                        || (enchantment.is(Enchantments.EFFICIENCY) && other.is(ModEnchantments.GREATER_EFFICIENCY)))
-            return true;
-        return Enchantment.areCompatible(enchantment, other);
+        return
+                allowEnchantments(enchantment, other, ModEnchantments.GREATER_EFFICIENCY, Enchantments.EFFICIENCY) ||
+                allowEnchantments(enchantment, other, ModEnchantments.GREATER_SHARPNESS, Enchantments.SHARPNESS) ||
+                Enchantment.areCompatible(enchantment, other);
+    }
+
+    @Unique
+    private static boolean allowEnchantments(
+            Holder<Enchantment> enchantment1,
+            Holder<Enchantment> enchantment2,
+            ResourceKey<Enchantment> enchantmentKey1,
+            ResourceKey<Enchantment> enchantmentKey2)
+    {
+        return (enchantment1.is(enchantmentKey1) && enchantment2.is(enchantmentKey2))
+                || (enchantment1.is(enchantmentKey2) && enchantment2.is(enchantmentKey1));
+    }
+
+    @Unique
+    private static void enchantmentsPrecedence(ItemEnchantments.Mutable enchantments, ResourceKey<Enchantment> bigger, ResourceKey<Enchantment> smaller) {
+        AtomicReference<Holder<Enchantment>> biggerHolder = new AtomicReference<>();
+        AtomicReference<Holder<Enchantment>> smallerHolder = new AtomicReference<>();
+
+        enchantments.keySet().forEach(holder -> {
+            if (holder.is(bigger)) {
+                biggerHolder.set(holder);
+            } else if (holder.is(smaller)) {
+                smallerHolder.set(holder);
+            }
+        });
+
+        if (biggerHolder.get() != null && smallerHolder.get() != null) {
+            enchantments.set(smallerHolder.get(), 0);
+        }
     }
 }
