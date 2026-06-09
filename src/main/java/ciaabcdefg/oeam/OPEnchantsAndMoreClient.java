fbfcc05a9d457e15df0d3fecc0ca46e288d646c7
ciaabcdefg <ciaabcdefg@gmail.com>
2026-06-09 16:00:36 +0700

    made client initializer more explicit

--- src/main/java/ciaabcdefg/oeam/OPEnchantsAndMoreClient.java
+++ src/main/java/ciaabcdefg/oeam/OPEnchantsAndMoreClient.java
@@ -3,8 +3,11 @@ package ciaabcdefg.oeam;
 import ciaabcdefg.oeam.particle.ModParticles;
 import ciaabcdefg.oeam.particle.custom.CoupDeGraceParticle;
 import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
 import net.fabricmc.fabric.api.client.particle.v1.ParticleProviderRegistry;
 
+@Environment(EnvType.CLIENT)
 public class OPEnchantsAndMoreClient implements ClientModInitializer {
     @Override
     public void onInitializeClient() {
