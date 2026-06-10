cbde7bc00af3a8899637d1b0593205857a0b7655
ciaabcdefg <ciaabcdefg@gmail.com>
2026-06-10 15:12:23 +0700

    move attribute mixin to new file

--- src/main/java/ciaabcdefg/oeam/mixin/ElytraMixin.java
+++ src/main/java/ciaabcdefg/oeam/mixin/ElytraMixin.java
@@ -13,14 +13,6 @@ import org.spongepowered.asm.mixin.injection.invoke.arg.Args;
 
 @Mixin(LivingEntity.class)
 public class ElytraMixin {
-    @ModifyReturnValue(
-            method = "createLivingAttributes",
-            at = @At("RETURN")
-    )
-    private static AttributeSupplier.Builder modifyAttributes(AttributeSupplier.Builder original) {
-        return original.add(ModAttributes.ELYTRA_SPEED_BONUS);
-    }
-
     @Unique
     private static double map(double value, double start1, double stop1, double start2, double stop2) {
         return start2 + (stop2 - start2) * ((value - start1) / (stop1 - start1));
