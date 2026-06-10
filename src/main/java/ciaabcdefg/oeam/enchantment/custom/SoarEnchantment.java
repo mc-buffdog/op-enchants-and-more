f66b204f8e9552876112dd8a30934fcd5c5d60f9
ciaabcdefg <ciaabcdefg@gmail.com>
2026-06-10 14:15:57 +0700

    add soar enchantment (for elytra)

---
+++ src/main/java/ciaabcdefg/oeam/enchantment/custom/SoarEnchantment.java
@@ -0,0 +1,40 @@
+package ciaabcdefg.oeam.enchantment.custom;
+
+import ciaabcdefg.oeam.OPEnchantsAndMore;
+import ciaabcdefg.oeam.attribute.ModAttributes;
+import ciaabcdefg.oeam.item.tag.ModItemTags;
+import net.minecraft.core.registries.Registries;
+import net.minecraft.data.worldgen.BootstrapContext;
+import net.minecraft.resources.Identifier;
+import net.minecraft.world.entity.EquipmentSlotGroup;
+import net.minecraft.world.entity.ai.attributes.AttributeModifier;
+import net.minecraft.world.item.enchantment.Enchantment;
+import net.minecraft.world.item.enchantment.EnchantmentEffectComponents;
+import net.minecraft.world.item.enchantment.LevelBasedValue;
+import net.minecraft.world.item.enchantment.effects.EnchantmentAttributeEffect;
+
+public class SoarEnchantment {
+    public static Enchantment.Builder build(BootstrapContext<Enchantment> context) {
+        var items = context.lookup(Registries.ITEM);
+        return Enchantment.enchantment(
+                        Enchantment.definition(
+                                items.getOrThrow(ModItemTags.FLYING_ENCHANTABLE),
+                                5,
+                                4,
+                                Enchantment.dynamicCost(1, 11),
+                                Enchantment.dynamicCost(21, 11),
+                                1,
+                                EquipmentSlotGroup.CHEST
+                        )
+                )
+                .withEffect(
+                        EnchantmentEffectComponents.ATTRIBUTES,
+                        new EnchantmentAttributeEffect(
+                                Identifier.fromNamespaceAndPath(OPEnchantsAndMore.MOD_ID, "soar"),
+                                ModAttributes.ELYTRA_SPEED_BONUS,
+                                LevelBasedValue.perLevel(2, 2.5F),
+                                AttributeModifier.Operation.ADD_VALUE
+                        )
+                );
+    }
+}
