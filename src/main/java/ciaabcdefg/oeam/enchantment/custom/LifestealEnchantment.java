8cd4d3ebfdbcab24622e7a7f5fb9a9d32bcd9515
ciaabcdefg <ciaabcdefg@gmail.com>
2026-06-10 15:12:30 +0700

    use lifesteal attribute instead of hardcode

--- src/main/java/ciaabcdefg/oeam/enchantment/custom/LifestealEnchantment.java
+++ src/main/java/ciaabcdefg/oeam/enchantment/custom/LifestealEnchantment.java
@@ -1,12 +1,17 @@
 package ciaabcdefg.oeam.enchantment.custom;
 
-import net.minecraft.core.HolderGetter;
+import ciaabcdefg.oeam.OPEnchantsAndMore;
+import ciaabcdefg.oeam.attribute.ModAttributes;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.data.worldgen.BootstrapContext;
+import net.minecraft.resources.Identifier;
 import net.minecraft.tags.ItemTags;
 import net.minecraft.world.entity.EquipmentSlotGroup;
-import net.minecraft.world.item.Item;
+import net.minecraft.world.entity.ai.attributes.AttributeModifier;
 import net.minecraft.world.item.enchantment.Enchantment;
+import net.minecraft.world.item.enchantment.EnchantmentEffectComponents;
+import net.minecraft.world.item.enchantment.LevelBasedValue;
+import net.minecraft.world.item.enchantment.effects.EnchantmentAttributeEffect;
 
 public final class LifestealEnchantment {
     public static Enchantment.Builder build(BootstrapContext<Enchantment> context) {
@@ -20,6 +25,14 @@ public final class LifestealEnchantment {
                         Enchantment.dynamicCost(21, 11),
                         1,
                         EquipmentSlotGroup.MAINHAND
+                )
+        ).withEffect(
+                EnchantmentEffectComponents.ATTRIBUTES,
+                new EnchantmentAttributeEffect(
+                        Identifier.fromNamespaceAndPath(OPEnchantsAndMore.MOD_ID, "lifesteal"),
+                        ModAttributes.LIFESTEAL,
+                        LevelBasedValue.perLevel(0.1F, 0.15F),
+                        AttributeModifier.Operation.ADD_VALUE
                 )
         );
     }
