3a27891d2d80ab082fca5d71b553b305f0c89cc1
ciaabcdefg <ciaabcdefg@gmail.com>
2026-06-14 17:06:55 +0700

    desolator remove parameter

--- src/main/java/ciaabcdefg/oeam/enchantment/effect/custom/DesolatorEnchantmentEffect.java
+++ src/main/java/ciaabcdefg/oeam/enchantment/effect/custom/DesolatorEnchantmentEffect.java
@@ -1,29 +1,19 @@
 package ciaabcdefg.oeam.enchantment.effect.custom;
 
 import ciaabcdefg.oeam.effect.ModEffects;
-import ciaabcdefg.oeam.sound.ModSounds;
+import ciaabcdefg.oeam.enchantment.custom.DesolatorEnchantment;
 import com.mojang.serialization.MapCodec;
-import com.mojang.serialization.codecs.RecordCodecBuilder;
 import net.minecraft.server.level.ServerLevel;
-import net.minecraft.sounds.SoundSource;
 import net.minecraft.world.effect.MobEffectInstance;
-import net.minecraft.world.effect.MobEffects;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.item.enchantment.EnchantedItemInUse;
-import net.minecraft.world.item.enchantment.LevelBasedValue;
 import net.minecraft.world.item.enchantment.effects.EnchantmentEntityEffect;
 import net.minecraft.world.phys.Vec3;
 import org.jspecify.annotations.NonNull;
 
-public record DesolatorEnchantmentEffect(LevelBasedValue amount) implements EnchantmentEntityEffect {
-    public static final MapCodec<DesolatorEnchantmentEffect> CODEC =
-            RecordCodecBuilder.mapCodec(instance ->
-            instance.group(
-                    LevelBasedValue.CODEC.fieldOf("amount").forGetter(DesolatorEnchantmentEffect::amount)
-            ).apply(instance, DesolatorEnchantmentEffect::new)
-    );
-
+public record DesolatorEnchantmentEffect() implements EnchantmentEntityEffect {
+    public static final MapCodec<DesolatorEnchantmentEffect> CODEC = MapCodec.unit(new DesolatorEnchantmentEffect());
 
     @Override
     public void apply(
@@ -34,12 +24,8 @@ public record DesolatorEnchantmentEffect(LevelBasedValue amount) implements Ench
         @NonNull Vec3 position
     ) {
         if (!(target instanceof LivingEntity victim)) return;
-        var desolatorInstance = new MobEffectInstance(ModEffects.DESOLATOR, 2 * 20, 0, false, true, true);
+        var desolatorInstance = new MobEffectInstance(ModEffects.DESOLATOR, Math.round(DesolatorEnchantment.DESOLATOR_DURATION * 20), 0, false, true, true);
         victim.addEffect(desolatorInstance);
-
-        if (item.owner() instanceof LivingEntity attacker) {
-            serverLevel.playSound(null, attacker.blockPosition(), ModSounds.DESOLATOR, SoundSource.PLAYERS, 0.8f, 1f);
-        }
     }
 
     @Override
