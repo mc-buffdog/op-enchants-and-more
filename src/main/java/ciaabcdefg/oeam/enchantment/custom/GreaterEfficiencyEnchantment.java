46127f5bf985505f9949cbda51d5fd9dd9e917f1
ciaabcdefg <ciaabcdefg@gmail.com>
2026-06-14 17:07:16 +0700

    add efficiency exclusive set

--- src/main/java/ciaabcdefg/oeam/enchantment/custom/GreaterEfficiencyEnchantment.java
+++ src/main/java/ciaabcdefg/oeam/enchantment/custom/GreaterEfficiencyEnchantment.java
@@ -1,6 +1,7 @@
 package ciaabcdefg.oeam.enchantment.custom;
 
 import ciaabcdefg.oeam.OPEnchantsAndMore;
+import ciaabcdefg.oeam.enchantment.tag.ModEnchantmentTags;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.data.worldgen.BootstrapContext;
 import net.minecraft.resources.Identifier;
@@ -16,6 +17,7 @@ import net.minecraft.world.item.enchantment.effects.EnchantmentAttributeEffect;
 public class GreaterEfficiencyEnchantment {
     public static Enchantment.Builder build(BootstrapContext<Enchantment> context) {
         var items = context.lookup(Registries.ITEM);
+        var enchantments = context.lookup(Registries.ENCHANTMENT);
         return Enchantment.enchantment(
                         Enchantment.definition(
                                 items.getOrThrow(ItemTags.MINING_ENCHANTABLE),
@@ -27,6 +29,7 @@ public class GreaterEfficiencyEnchantment {
                                 EquipmentSlotGroup.MAINHAND
                         )
                 )
+                .exclusiveWith(enchantments.getOrThrow(ModEnchantmentTags.EFFICIENCY_EXCLUSIVE))
                 .withEffect(
                         EnchantmentEffectComponents.ATTRIBUTES,
                         new EnchantmentAttributeEffect(
